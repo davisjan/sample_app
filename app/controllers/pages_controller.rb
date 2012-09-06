aae389ccd3d459b92e15608aed2521514ef60509
Janet Davis <davisjan@cs.grinnell.edu>
2012-09-06 13:49:10 -0500

    Made pages title a variable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,15 @@ class PagesController < ApplicationController
   # This code is called before rendering the view
 
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def about
+    @title = "About"
   end
 
 end
