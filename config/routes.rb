eaf42d2b31b9845eefea842c6fa403fb3fa8a2a8
Janet Davis <davisjan@cs.grinnell.edu>
2012-09-12 17:03:11 -0500

    Added routes for new user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,15 @@ SampleApp::Application.routes.draw do
 
   get "users/new"
 
+  match '/signup', :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
 
-  resources :widgets
+  root :to => "pages#home"
 
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
-  get "pages/help"
+  resources :widgets
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -60,9 +59,6 @@ SampleApp::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
