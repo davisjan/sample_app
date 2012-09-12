b83dcc7d504470653bf1ec3b6be28232949a227f
Janet Davis <davisjan@cs.grinnell.edu>
2012-09-12 17:17:54 -0500

    Removed TMI from page title tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,6 +16,7 @@ describe PagesController do
       get 'home'
       response.should have_selector("title", 
                                     :content => @base_title + " | Home")
+      response.should have_selector("title", :content => "Home")
     end
   end
 
@@ -26,8 +27,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", 
-                                    :content => @base_title + " | Contact")
+      response.should have_selector("title", :content => "Contact")
     end
   end
 
@@ -38,8 +38,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", 
-      	                            :content => @base_title + " | About")
+      response.should have_selector("title", :content => "About")
     end
   end
 
