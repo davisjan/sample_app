2337cf487a9b769c7178f83f1867da02535de51b
Janet Davis <davisjan@cs.grinnell.edu>
2012-09-13 11:57:43 -0500

    Enforce unique email addresses with ActiveRecords validators and a database index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120913160756) do
+ActiveRecord::Schema.define(:version => 20120913165559) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(:version => 20120913160756) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
   create_table "widgets", :force => true do |t|
     t.string   "name"
