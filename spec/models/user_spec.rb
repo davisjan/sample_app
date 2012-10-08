c70214d69ce8358dc6f4e07b44f1b84eeafe376f
Janet Davis <davisjan@cs.grinnell.edu>
2012-10-08 14:58:39 -0500

    Impemented followers method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -241,5 +241,20 @@ describe User do
       @user.unfollow! @followed
       @user.should_not be_following @followed
     end
+    it "should have a reverse_relationships method" do
+      @user.should respond_to :reverse_relationships
+    end
+    it "should have a followers method" do
+      @user.should respond_to :followers
+    end
+    it "should include the follower in the followers array" do
+      @user.follow! @followed
+      @followed.followers.should include @user
+    end
+    it "should destroy associated reverse relationships" do
+      @user.follow! @followed
+      @followed.destroy
+      Relationship.find_by_followed_id(@followed.id).should be_nil
+    end
   end
 end
